a51e2bfc4ab94fa3b087a8a4511c1482687f7f38
dominguesgabe <gabrieldomingues522@gmail.com>
2023-10-10 19:23:05 -0300

    applied getServerSideProps

--- src/pages/product/[slug]/index.tsx
+++ src/pages/product/[slug]/index.tsx
@@ -1,24 +1,27 @@
 import { useState, useEffect, MouseEvent } from "react"
-import { useRouter } from "next/router"
 import { apiService } from "@/services"
 import { Product } from "@/types"
 import { StorageObjectItem } from "@/factories"
 import { StorageItem } from "@/types"
 import { cartUtils } from "@/utils"
 
-export default function Product() {
-  const [product, setProduct] = useState<Product>()
-  const [addCartQuantity, setAddCartQuantity] = useState(1)
-  const router = useRouter()
+export async function getServerSideProps(context) {
+  const productId = context.query.slug
 
-  useEffect(() => {
-    const productId = Number(router.query.slug)
-    if (productId && !isNaN(productId)) {
-      apiService.show(productId).then((productArray) => {
-        setProduct(productArray[0])
-      })
-    }
-  }, [router.query])
+  const response = await fetch(`http://localhost:3004/products?id=${productId}`)
+
+  const product = await response.json()
+
+  if (!product[0]) return { notFound: true }
+
+  return { props: { data: product[0] } }
+}
+interface ProductPageProps {
+  product: Product
+}
+
+export default function Product({ product }: ProductPageProps) {
+  const [addCartQuantity, setAddCartQuantity] = useState(1)
 
   function addToCart() {
     const storageCart = localStorage.getItem("cart") ?? "[]"
@@ -28,20 +31,20 @@ export default function Product() {
 
     if (actualCart.length) {
       const isItemAlreadyInCart = actualCart.some(
-        (item) => item.productId === product?.id
+        (item) => item.productId === product.id
       )
 
       if (isItemAlreadyInCart) {
         newCart = cartUtils.quantityModifier({
           cart: actualCart,
-          targetProductId: product?.id ?? 1,
+          targetProductId: product.id,
           newQuantity: 1,
-        }) //targetProductId can cause bugs, todo fix
+        })
       } else {
-        newCart = [...actualCart, new StorageObjectItem(product!.id, 1)]
+        newCart = [...actualCart, new StorageObjectItem(product.id, 1)]
       }
     } else {
-      newCart = [new StorageObjectItem(product!.id, 1)]
+      newCart = [new StorageObjectItem(product.id, 1)]
     }
 
     localStorage.setItem("cart", JSON.stringify(newCart))
@@ -52,24 +55,21 @@ export default function Product() {
   }
 
   return (
-    product && (
-      <main className={`flex min-h-screen flex-col items-center p-24`}>
-        <p>url id: {router.query.slug}</p>
-        <p>product name: {product.name}</p>
-        <p>product description: {product.description}</p>
-        <p>product price: {product.price}</p>
-        <img
-          className="w-60"
-          src={"http://localhost:3004" + product.image_url}
-          alt={product.name}
-        />
-        <div>
+    <main className={`flex min-h-screen flex-col items-center p-24`}>
+      <p>product name: {product.name}</p>
+      <p>product description: {product.description}</p>
+      <p>product price: {product.price}</p>
+      <img
+        className="w-60"
+        src={"http://localhost:3004" + product.image_url}
+        alt={product.name}
+      />
+      {/* <div>
           <button onClick={handleChangeItem}>-</button>
           <input name="inputQuantity" value={addCartQuantity} type="number" />
           <button onClick={handleChangeItem}>+</button>
-        </div>
-        <button onClick={addToCart}>Add to cart</button>
-      </main>
-    )
+        </div> */}
+      <button onClick={addToCart}>Add to cart</button>
+    </main>
   )
 }
